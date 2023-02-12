ed6585dadd807efa19a22b1b1342a07372a00cbd
Patrick Flores <savvyscheme@gmail.com>
2023-02-11 20:00:30 -0700

    Updated Database connection

--- app/config/ContainerConfig.php
+++ app/config/ContainerConfig.php
@@ -3,6 +3,9 @@
 namespace App\Config;
 
 use Pimple\Container;
+
+use App\Database\DBAlConnection;
+
 use Bramus\Router\Router;
 use App\Routes\API;
 
@@ -12,12 +15,15 @@ class ContainerConfig {
     {
         $container = new Container();
 
+        // DI for Databse
+        $container['database_connection'] = new DBAlConnection($_ENV["DB_DRIVER"], $_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
+        
+
         // DI for routes
         $container["router"] = fn($c) => new Router();
         $container["routes"] = fn($c) => new API($c["router"]);
 
 
-        
         return $container;
 
     }
---
+++ app/src/database/DBAlConnection.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Database;
+
+use Doctrine\DBAL\DriverManager;
+
+class DBAlConnection {
+    private $connection;
+
+    public function __construct($db_driver, $db_host, $db_name, $username, $password) {
+
+        $connectionParams = [
+            'dbname' => $db_name,
+            'user' => $username,
+            'password' => $password,
+            'host' => $db_host,
+            'driver' => $db_driver ?? 'pdo_mysql',
+        ];
+
+        return $this->connection = DriverManager::getConnection($connectionParams);
+        
+    }
+}
