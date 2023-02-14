8d38d387462fa36696afcff5f75957a3fe71b809
Patrick Flores <savvyscheme@gmail.com>
2023-02-13 17:15:26 -0700

    Added User DAO and implementation

--- app/src/_DAO/UserDAO.php
+++ app/src/_DAO/UserDAO.php
@@ -1,24 +1,6 @@
 <?php
 namespace App\DAO;
 
-class UserDAO {
-    private $connection;
-
-    public function __construct($connection) {
-        $this->connection = $connection;
-    }
-
-    public function get_all_users() {
-        $queryBuilder = $this->connection->createQueryBuilder();
-        $queryBuilder
-            ->select('MEDREC_ID', 'FIRSTNAME', 'LASTNAME')
-            ->from('patient', 'p')
-            // ->join('p', 'medication', 'm', 'p.MEDREC_ID = m.MEDREC_ID')
-            // ->where('PRESCRIBED_COUNT >')
-            // ->setParameter(0)
-            ->setMaxResults(20);
-
-        $userArray = $queryBuilder->execute()->fetchAll();
-
-    }
+interface UserDao {
+    public function get_all_users();
 }
---
+++ app/src/_DAO/UserDaoImpl.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\DAO;
+use App\DAO\UserDao;
+
+class UserDaoImpl implements UserDAO{
+    
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function get_all_users() {
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('MEDREC_ID', 'FIRSTNAME', 'LASTNAME')
+            ->from('patient', 'p')
+            // ->join('p', 'medication', 'm', 'p.MEDREC_ID = m.MEDREC_ID')
+            // ->where('PRESCRIBED_COUNT >')
+            // ->setParameter(0)
+            ->setMaxResults(20);
+
+        $userArray = $queryBuilder->execute()->fetchAll();
+
+    }
+}
