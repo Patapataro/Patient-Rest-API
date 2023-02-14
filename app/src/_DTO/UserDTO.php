59da3b305fa54698047b05eea5ccf7d93f561474
Patrick Flores <savvyscheme@gmail.com>
2023-02-13 17:12:34 -0700

    Added UserDTO get and set methods

--- app/src/_DTO/UserDTO.php
+++ app/src/_DTO/UserDTO.php
@@ -1,8 +1,41 @@
-<?php
+<?php declare(strict_types=1);
 namespace App\DTO;
 
 class UserDTO {
     private $name;
-    private $id;
-    private $NDCs;
+    private $ID;
+    private $NDC;
+
+    // Name get and set
+    public function get_name(): string
+    {
+        return $this->name;
+    }
+
+    public function set_name(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    // ID get and set
+    public function get_id(): int
+    {
+        return $this->ID;
+    }
+
+    public function set_id(int $ID): void
+    {
+        $this->ID = $ID;
+    }
+
+    // NDC get and set
+    public function get_ndc(): string
+    {
+        return $this->NDC;
+    }
+
+    public function set_ndc(string $NDC): void
+    {
+        $this->NDC = $NDC;
+    }
 }
