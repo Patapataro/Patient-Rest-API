3a300ac0bd8dd3b91563025fba2a0bf6f07f9e55
Patrick Flores <savvyscheme@gmail.com>
2023-02-18 18:08:24 -0700

    Added DTO mapping to PatientService

--- app/src/services/PatientService.php
+++ app/src/services/PatientService.php
@@ -17,18 +17,51 @@ class PatientService
     public function getPatient($id)
     {
         $patient = $this->PatientDAO->read($id);
-        return json_encode($patient);
+        $patientDTO = $this::arrToDto($patient);
+
+        return json_encode($patientDTO);
     }
 
     public function getPatients()
     {
         $patients = $this->PatientDAO->getAll();
-        return json_encode($patients);
+        $patientDTOs = $this::mapPatientArray($patients);
+
+        return json_encode($patientDTOs);
     }
 
     public function patientPrescribedCount($n)
     {
         $patients = $this->PatientDAO->prescribedGtN($n);
-        return json_encode($patients);
+        $patientDTOs = $this::mapPatientArray($patients);
+
+        return json_encode($patientDTOs);
+    }
+       
+    /**
+     * Maps a patent arrays to array of patent DTO
+     */
+    private static function mapPatientArray($userArrays): array
+    {
+        $userDTOs = array_map(function($userArray) {
+            $PatientDTO = new PatientDTO();
+            $PatientDTO->ID = $userArray['ID'];
+            $PatientDTO->name = $userArray['NAME'];
+            return $PatientDTO;
+        }, $userArrays);
+
+        return $userDTOs;
+    }
+
+    /**
+     * Maps a single patient array to a patent DTO
+     */
+    public static function arrToDto($userArray): PatientDTO
+    {
+        $PatientDTO = new PatientDTO();
+        $PatientDTO->ID = $userArray['ID'];
+        $PatientDTO->name = $userArray['NAME'];
+
+        return $PatientDTO;
     }
 }
